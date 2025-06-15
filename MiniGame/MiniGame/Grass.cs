f7ffe1e9be2a590333b69a783b127417a2e7a776
Vlad4526 <vladosik15473@gmail.com>
2025-06-15 14:56:36 +0300

    Add files via upload

---
+++ MiniGame/MiniGame/Grass.cs
@@ -0,0 +1,9 @@
+﻿public class Grass
+{
+    public int Вік { get; private set; } = 0;
+
+    public void Rost()
+    {
+        Вік++;
+    }
+}
---
+++ MiniGame/MiniGame/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Map карта = new Map(12, 12);
+        карта.Ініціалізація();
+
+        for (int день = 1; день <= 5; день++)
+        {
+            Console.WriteLine($"День {день}");
+            карта.СимулюватиДень();
+            карта.ВивестиКарту();
+            Console.WriteLine();
+
+            if (карта.КінецьГри())
+            {
+                break;
+            }
+
+            Console.WriteLine("Натисніть будь-яку клавішу для продовження до наступного дня...");
+            Console.ReadKey();
+        }
+
+        Console.WriteLine("Кінець гри");
+    }
+}
